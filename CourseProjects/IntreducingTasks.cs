1c2fbae3a8adf5d4540524bd9042f4cc905f4e0a
BakPeter <bak.pet@gmail.com>
2022-02-18 18:33:53 +0200

    Section 1: Task Programming

---
+++ CourseProjects/IntreducingTasks.cs
@@ -0,0 +1,75 @@
+﻿//using System;
+
+//namespace CourseProjects
+//{
+//    internal class IntreducingTasks
+//    {
+//        private static void Write(char c)
+//        {
+//            int i = 1000;
+//            while (i-- > 0)
+//            {
+//                Console.Write(c);
+//            }
+//        }
+//        private static void Write(object o)
+//        {
+//            int i = 1000;
+//            while (i-- > 0)
+//            {
+//                Console.Write(o.ToString());
+//            }
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            //CreateAndStartASimpleTasks();
+//            //TasksWithState();
+//            TasksWithReturnValue();
+
+//            Console.WriteLine("Main program done, press any key.");
+//            Console.ReadKey();
+//        }
+
+//        private static void TasksWithReturnValue()
+//        {
+//            string text1 = "testing", text2 = "this";
+//            var t1 = new Task<int>(TextLength, text1);
+//            t1.Start();
+//            var t2 = Task.Factory.StartNew(TextLength, text2);
+
+//            Console.WriteLine(t1.Result);
+//            Console.WriteLine(t2.Result);
+
+//        }
+
+//        private static int TextLength(object o)
+//        {
+//            Console.WriteLine($"\nTask with id {Task.CurrentId} processing object '{o}'...");
+//            return o.ToString().Length;
+//        }
+
+//        private static void TasksWithState()
+//        {
+//            Task t = new Task(Write, "foo");
+//            t.Start();
+
+//            Task.Factory.StartNew(Write, "bar");
+       
+//        }
+
+//        private static void CreateAndStartASimpleTasks()
+//        {
+//            Task.Factory.StartNew(() => {
+//                Write('-');
+//            });
+
+//            Task t = new Task(() => Write('?'));
+//            t.Start();
+
+//            Write('.');
+//        }
+
+        
+//    }
+//}
---
+++ CourseProjects/WaitingForATimeToPass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProjects
+{
+    internal class WaitingForATimeToPass
+    {
+        public static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var t = new Task(() => 
+            {
+                Console.WriteLine("You have 5 secondes to disarm the bomb by poressing any key");
+                bool canceled = token.WaitHandle.WaitOne(5000);
+                Console.WriteLine(canceled ? "Bomb disarmed" : "BOOM!!!!");
+            }, token);
+            t.Start();
+
+            Thread.SpinWait(10000);
+            SpinWait.SpinUntil(() => false);
+            Console.WriteLine("Are you still here?");
+
+            Console.ReadKey();
+            cts.Cancel();
+
+            Console.WriteLine("Main program is done, press any key");
+            Console.ReadKey();
+        }
+    }
+}
