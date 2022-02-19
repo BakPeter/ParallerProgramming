1933f3c7c95dc019388c4617f5dec7b1d7e35ffa
BakPeter <bak.pet@gmail.com>
2022-02-19 13:53:43 +0200

    Section 2: Data Sharing and Synchronization

---
+++ Section_2/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿namespace Section_2
+{
+    public class BankAccount
+    {
+        //public object padlock = new object();
+     
+        private int balance;
+        public int Balance { get => balance; private set => balance = value; }
+
+        public void Deposit(int amount)
+        {
+            //lock (padlock)
+            //{
+            //    // += is really two operations
+            //    // op1 is temp <- get_Balance() + amount
+            //    // op2 is set_Balance(temp)
+            //    // something can happen _between_ op1 and op2
+            //    Balance += amount;
+            //}
+
+            Interlocked.Add(ref balance, amount);
+        }
+
+        public void Withdraw(int amount)
+        {
+            //lock (padlock)
+            //{
+            //    Balance -= amount;
+            //}
+
+
+            Interlocked.Add(ref balance, -amount);
+        }
+
+        // show interlocked methods here
+
+        // Interlocked.MemoryBarrier is a wrapper for Thread.MemoryBarrier
+        // only required on memory systems that have weak memory ordering (e.g., Itanium)
+        // prevents the CPU from reordering the instructions such that those before the barrier
+        // execute after those after
+    }
+}
